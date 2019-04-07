697be9bb395ddd98bcd3b97ecffbe80dc5fa1e74
Iran Marinho <ir4nbala@gmail.com>
2019-04-07 07:21:48 -0300

    Calc
    Basic calc

---
+++ calc.js
@@ -0,0 +1,20 @@
+function mult (x, y) {
+    console.log( x * y)
+}
+
+function div (x, y) {
+    console.log( x / y)
+}
+
+function sum (x, y) {
+    console.log( x + y)
+}
+
+function sub (x, y) {
+    console.log( x - y)
+}
+
+mult(3, 5)
+div(10, 2)
+sum(4, 6)
+sub(10, 5)
