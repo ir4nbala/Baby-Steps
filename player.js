59f8689627acdbba4fa94cc5df568e62342efec0
Iran Marinho <ir4nbala@gmail.com>
2019-04-07 06:37:20 -0300

    Player attributes
    add player attributes Object

---
+++ player.js
@@ -0,0 +1,31 @@
+let player = {
+    name: "",
+    level: 0,
+    health: 0,
+    mana: 0,
+    capacity: 0,
+    magic: 0,
+    melee: 0,
+    distance: 0,
+    defense: 0,
+    spirit: 0,
+    armor: 0,
+    block: 0,
+    crit: 0,
+}
+
+let warrior = player
+warrior.name = "Arthur"
+warrior.level = 35
+warrior.health = 450
+warrior.mana = 100
+warrior.capacity = 350
+warrior.magic = 3
+warrior.melee = 45
+warrior.defense = 50
+warrior.armor = 9
+warrior.block = 15
+warrior.crit = 5
+
+
+console.log(warrior)
